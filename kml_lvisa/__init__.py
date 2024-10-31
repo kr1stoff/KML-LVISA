7ab67f6961b8bc0408c08643c6139eff7e9d7250
kristoff <706060951@qq.com>
2024-10-31 10:28:02 +0800

    241031

--- kml_lvisa/__init__.py
+++ kml_lvisa/__init__.py
@@ -1,2 +1,2 @@
-from .fastq import prepare_fastq_by_sample_table
-from .fastq import get_sample_names_by_sample_table
+from .fastq import prepare_fastq_by_samptab
+from .fastq import get_sample_names_by_samptab
--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -1,13 +1,13 @@
-from kml_lvisa import prepare_fastq_by_sample_table
-from kml_lvisa import get_sample_names_by_sample_table
+from kml_lvisa import prepare_fastq_by_samptab
+from kml_lvisa import get_sample_names_by_samptab
 
 work_dir = '/data/mengxf/Project/KML240924_lvis_pipeline/result/241031'
 sample_table = '/data/mengxf/GitHub/KML-LVISA/templates/input.tsv'
 
 
-def test_prepare():
-    prepare_fastq_by_sample_table(work_dir, sample_table)
+# def test_prepare():
+#     prepare_fastq_by_samptab(work_dir, sample_table)
 
 
 def test_get():
-    get_sample_names_by_sample_table(sample_table)
+    get_sample_names_by_samptab(sample_table)
