e946d645df8e2870fcc97aec892ec9732261d28d
kristoff <706060951@qq.com>
2025-07-17 16:33:03 +0800

    Add summary and per-sample stats for CpG, TSS, Effect, Repeat
    Introduced scripts and workflow rules to generate per-sample summary tables for CpG and TSS regions, as well as for Effect and Repeat class statistics. Updated filter logic in isite coverage script to exclude entries with UMI == 0. Modified Snakefile and bigrprt.smk to include new summary outputs and ensure all relevant statistics are generated and tracked in the workflow.

---
+++ scripts/summary_cpg_tss.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import sys
+from pathlib import Path
+
+
+annotabs = sys.argv[3:]
+cpg_outfile = sys.argv[1]
+tss_outfile = sys.argv[2]
+
+# annotabs = list(Path('/data/mengxf/Project/KML250703-lvis-pipeline-update/result/250714/anno').glob('*.tsv'))
+annotabs = [x for x in annotabs if ('POS' not in str(x)) and ('NTC' not in str(x))]
+
+# CpG
+df_all = pd.DataFrame(columns=['Sample', 'CpG1KB', 'CpG2.5KB', 'CpG5KB', 'CpG10KB'])
+for annotab in annotabs:
+    df = pd.read_csv(annotab, sep='\t', usecols=['CpG1KB', 'CpG2.5KB', 'CpG5KB', 'CpG10KB'])
+    df['Sample'] = Path(annotab).stem.split('.')[0]
+    df_all = pd.concat([df_all, df])
+# 筛选 CpG1KB 和 CpG2.5KB 列，统计非 '-' 的数量
+result = (
+    df_all.groupby('Sample')
+    .apply(lambda x: pd.Series({
+        'CpG1KB': (x['CpG1KB'] != '-').sum(),
+        'CpG2.5KB': (x['CpG2.5KB'] != '-').sum(),
+        'CpG5KB': (x['CpG5KB'] != '-').sum(),
+        'CpG10KB': (x['CpG10KB'] != '-').sum(),
+    }), include_groups=False)
+    .reset_index()
+)
+result.to_csv(cpg_outfile, index=False, sep='\t')
+
+# TSS
+df_all = pd.DataFrame(columns=['Sample', 'TSS1KB', 'TSS2.5KB', 'TSS5KB', 'TSS10KB'])
+for annotab in annotabs:
+    df = pd.read_csv(annotab, sep='\t', usecols=['TSS1KB', 'TSS2.5KB', 'TSS5KB', 'TSS10KB'])
+    df['Sample'] = Path(annotab).stem.split('.')[0]
+    df_all = pd.concat([df_all, df])
+# 筛选 TSS1KB 和 TSS2.5KB 列，统计非 '-' 的数量
+result = (
+    df_all.groupby('Sample')
+    .apply(lambda x: pd.Series({
+        'TSS1KB': (x['TSS1KB'] != '-').sum(),
+        'TSS2.5KB': (x['TSS2.5KB'] != '-').sum(),
+        'TSS5KB': (x['TSS5KB'] != '-').sum(),
+        'TSS10KB': (x['TSS10KB'] != '-').sum(),
+    }), include_groups=False)
+    .reset_index()
+)
+result.to_csv(tss_outfile, index=False, sep='\t')
